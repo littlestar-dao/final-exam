82fa4d255402b2379729764d93c19007d996a886
littlestar-dao <dao.duan@hotmail.com>
2017-08-04 17:05:29 +0700

    buy A=1,B=1,C=1 should return 270

--- Final02/src/main/java/Final02.java
+++ Final02/src/main/java/Final02.java
@@ -2,26 +2,36 @@ public class Final02 {
     private double price;
     private int buyA;
     private int buyB;
+    private int buyC;
     private String pA;
     private String pB;
-    public Final02(String firstProduct, String secondProduct) {
-        this.pA = firstProduct;
-        this.pB = secondProduct;
+    private String pC;
+
+    public Final02(String product1, String product2, String product3) {
+        this.pA = product1;
+        this.pB = product2;
+        this.pC = product3;
     }
 
     public void productA() {
         buyA++;
     }
+
     public void productB() {
         buyB++;
     }
 
-    public double getResult(){
-        if (buyA ==1 && buyB ==1){
-            price = (buyA+buyB) *100 *0.95;
-        }else{
-            price = 100*buyA;
+    public void productC() {
+        buyC++;
+    }
 
+    public double getResult() {
+        if (buyA == 1 && buyB == 1 && buyC == 1) {
+            price = (buyA + buyB + buyC) * 100 * 0.9;
+        } else if (buyA == 1 && buyB == 1) {
+            price = (buyA + buyB) * 100 * 0.95;
+        } else {
+            price = 100 * buyA;
         }
         return price;
     }
--- Final02/src/test/java/Final02Test.java
+++ Final02/src/test/java/Final02Test.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Final02Test {
-    Final02 final02 = new Final02("A", "B");
+    Final02 final02 = new Final02("A", "B","C");
     @Test
     public void buy_A_1_should_return_100() {
         final02.productA();
@@ -17,6 +17,15 @@ public class Final02Test {
         final02.productB();
         double actualResult = final02.getResult();
         assertEquals( 190,actualResult,0);
+    }
+
+    @Test
+    public void buy_A_1_B_1_C_1should_return_270() {
+        final02.productA();
+        final02.productB();
+        final02.productC();
+        double actualResult = final02.getResult();
+        assertEquals( 270,actualResult,0);
     }
 
 }
