6f4bed5afb02c6c8c511b85108d14ff8ebea45c3
littlestar-dao <dao.duan@hotmail.com>
2017-08-04 17:17:15 +0700

    buy A=1,B=1.C=1,D=1,E=1 should return 375

--- Final02/src/main/java/Final02.java
+++ Final02/src/main/java/Final02.java
@@ -4,16 +4,19 @@ public class Final02 {
     private int buyB;
     private int buyC;
     private int buyD;
+    private int buyE;
     private String pA;
     private String pB;
     private String pC;
     private String pD;
+    private String pE;
 
-    public Final02(String product1, String product2, String product3,String product4) {
+    public Final02(String product1, String product2, String product3, String product4, String product5) {
         this.pA = product1;
         this.pB = product2;
         this.pC = product3;
         this.pD = product4;
+        this.pE = product5;
     }
 
     public void productA() {
@@ -27,15 +30,21 @@ public class Final02 {
     public void productC() {
         buyC++;
     }
+
     public void productD() {
         buyD++;
     }
 
+    public void productE() {
+        buyE++;
+    }
+
     public double getResult() {
-        if(buyA == 1 && buyB ==1 && buyC ==1 && buyD ==1){
-            price =(buyA+buyB+buyC+buyD) * 100 *0.8;
-        }
-        else if (buyA == 1 && buyB == 1 && buyC == 1) {
+        if (buyA == 1 && buyB == 1 && buyC == 1 && buyD == 1 && buyE == 1) {
+            price = (buyA + buyB + buyC + buyD + buyE) * 100 * 0.75;
+        } else if (buyA == 1 && buyB == 1 && buyC == 1 && buyD == 1) {
+            price = (buyA + buyB + buyC + buyD) * 100 * 0.8;
+        } else if (buyA == 1 && buyB == 1 && buyC == 1) {
             price = (buyA + buyB + buyC) * 100 * 0.9;
         } else if (buyA == 1 && buyB == 1) {
             price = (buyA + buyB) * 100 * 0.95;
--- Final02/src/test/java/Final02Test.java
+++ Final02/src/test/java/Final02Test.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Final02Test {
-    Final02 final02 = new Final02("A", "B","C", "D");
+    Final02 final02 = new Final02("A", "B","C", "D","E");
     @Test
     public void buy_A_1_should_return_100() {
         final02.productA();
@@ -36,6 +36,17 @@ public class Final02Test {
         final02.productD();
         double actualResult = final02.getResult();
         assertEquals( 320,actualResult,0);
+    }
+
+    @Test
+    public void buy_A_1_B_1_C_1_D_1_E_1should_return_425() {
+        final02.productA();
+        final02.productB();
+        final02.productC();
+        final02.productD();
+        final02.productE();
+        double actualResult = final02.getResult();
+        assertEquals( 375,actualResult,0);
     }
 
 }
