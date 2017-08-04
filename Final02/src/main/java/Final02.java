92988737ef86a88e436bcfa34e2621e9aa9f8db3
littlestar-dao <dao.duan@hotmail.com>
2017-08-04 16:55:48 +0700

    if buy A 1 piece and B 1 piece should return 190

--- Final02/src/main/java/Final02.java
+++ Final02/src/main/java/Final02.java
@@ -1,9 +1,29 @@
 public class Final02 {
-    private int price;
-    public int getResult(String productType,int piece){
-        if(productType == "A"){
-            price = 100*piece;
+    private double price;
+    private int buyA;
+    private int buyB;
+    private String pA;
+    private String pB;
+    public Final02(String firstProduct, String secondProduct) {
+        this.pA = firstProduct;
+        this.pB = secondProduct;
+    }
+
+    public void productA() {
+        buyA++;
+    }
+    public void productB() {
+        buyB++;
+    }
+
+    public double getResult(){
+        if (buyA ==1 && buyB ==1){
+            price = (buyA+buyB) *100 *0.95;
+        }else{
+            price = 100*buyA;
+
         }
         return price;
     }
+
 }
--- Final02/src/test/java/Final02Test.java
+++ Final02/src/test/java/Final02Test.java
@@ -3,12 +3,20 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Final02Test {
-    Final02 final02 = new Final02();
+    Final02 final02 = new Final02("A", "B");
+    @Test
+    public void buy_A_1_should_return_100() {
+        final02.productA();
+        double actualResult = final02.getResult();
+        assertEquals( 100,actualResult,0);
+    }
 
     @Test
-    public void input_A_1_should_return_input() {
-        int actualResult = final02.getResult("A",1);
-        assertEquals( 100,actualResult);
-
+    public void buy_A_1_B_1_should_return_190() {
+        final02.productA();
+        final02.productB();
+        double actualResult = final02.getResult();
+        assertEquals( 190,actualResult,0);
     }
+
 }
